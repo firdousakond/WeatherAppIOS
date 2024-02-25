bfdb2006482e24a295dd36746ab27c5804a23ba4
FirdousAkond <shahanifirdous@gmail.com>
2024-02-25 22:58:00 +0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Firdous Shahani on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
